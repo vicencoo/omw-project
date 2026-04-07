749d4fd4ba666386bfe72f16ecd482aa598117a5
“vicencoo” <vicencollanaj2004@gmail.com>
2026-04-07 16:27:24 +0200

    updated mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,31 +1,39 @@
 import { Menu } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { HeaderItems } from '../constants/headerItems';
 import { useDisclosure } from '../hooks/useDisclosure';
 import { Text } from './Text';
 
 export const MobileMenu = ({ t }) => {
   const { toggle, close, isOpen, ref: wrapperRef } = useDisclosure();
-  const navigate = useNavigate();
+
   return (
-    <div className='flex md:hidden relative ' ref={wrapperRef}>
-      <Menu onClick={toggle} size={27} />
+    <div className='flex md:hidden relative' ref={wrapperRef}>
+      <Menu onClick={toggle} size={27} className='cursor-pointer' />
 
       <div
-        className={`fixed top-18 right-4 z-2200 bg-white flex w-42.5 flex-col py-3 rounded-lg px-4 gap-3 shadow-sm shadow-green-100 ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5 pointer-events-none'} transition-all duration-300 will-change-transform`}
+        className={`fixed top-18 right-4 z-2200 bg-white flex w-42.5 flex-col py-3 rounded-lg px-4 gap-3 shadow-sm shadow-green-100 ${
+          isOpen
+            ? 'opacity-100 translate-y-0'
+            : 'opacity-0 -translate-y-5 pointer-events-none'
+        } transition-all duration-300 will-change-transform`}
       >
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-2'>
           {HeaderItems.map((item) => (
-            <span
+            <NavLink
               key={item.id}
-              className='uppercase text-sm font-medium tracking-wide text-gray-600 hover:text-green-700 cursor-pointer transition-colors duration-200'
-              onClick={() => {
-                navigate(item.path);
-                close();
-              }}
+              to={item.path}
+              onClick={close}
+              className={({ isActive }) =>
+                `uppercase text-sm font-medium tracking-wide rounded-md p-3 transition-colors duration-200 ${
+                  isActive
+                    ? 'text-green-700  border-b border-green-200'
+                    : 'text-gray-600 hover:text-green-700 hover:border-b border-green-200'
+                }`
+              }
             >
               <Text text={t(item.label)} size='text-sm' font='font-medium' />
-            </span>
+            </NavLink>
           ))}
         </div>
       </div>
