96865e2d61898518bf835a7d8c68719fc00b1d6f
“vicencoo” <vicencollanaj2004@gmail.com>
2026-04-13 17:07:52 +0200

    updated supabase

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,30 @@
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+
+//   {
+//     dialect: 'mysql',
+//     host: process.env.DB_HOST,
+//   },
+// );
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-
-  {
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.SUPABASE_DB_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-);
+});
 
 module.exports = sequelize;
